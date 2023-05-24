922915dcdcd16f133611e954c84a3cc57ec12817
Thalysson <utaliidesenv@gmail.com>
2023-05-24 12:15:55 -0300

    update(): img in chat

--- src/env.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    url: {
-        API_URL: window.location.href.includes("localhost") ?
-            "http://localhost:7001" :
-            null
-    }
-};
-
-export default config;
